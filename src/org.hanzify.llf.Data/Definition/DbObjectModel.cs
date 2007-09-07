42c3363e67fff90e62f58592e54f5f080fd927b5
SND\lifeng_cp <SND\lifeng_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2007-09-07 07:58:15 +0000

    #12631
    git-svn-id: https://dbentry.svn.codeplex.com/svn@26625 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- src/org.hanzify.llf.Data/Definition/DbObjectModel.cs
+++ src/org.hanzify.llf.Data/Definition/DbObjectModel.cs
@@ -11,7 +11,7 @@ using org.hanzify.llf.Data.QuerySyntax;
 namespace org.hanzify.llf.Data.Definition
 {
     [Serializable]
-    public class DbObjectModel<T, TKey> : DbObjectSmartUpdate
+    public class DbObjectModel<T, TKey> : DbObjectModelBase<T, TKey>
     {
         [DbKey, DbColumn("Id")]
         protected internal TKey m_Id;
@@ -20,108 +20,6 @@ namespace org.hanzify.llf.Data.Definition
         public TKey Id
         {
             get { return m_Id; }
-        }
-
-        protected static CK Col
-        {
-            get { return CK.Column; }
-        }
-
-        public void Save()
-        {
-            DbEntry.Save(this);
-        }
-
-        public void Delete()
-        {
-            DbEntry.Delete(this);
-        }
-
-        public ValidateHandler Validate()
-        {
-            ValidateHandler v = new ValidateHandler();
-            v.ValidateObject(this);
-            return v;
-        }
-
-        public bool IsValid()
-        {
-            return Validate().IsValid;
-        }
-
-        public static T FindById(TKey Id)
-        {
-            return DbEntry.GetObject<T>(Id);
-        }
-
-        public static DbObjectList<T> FindBySql(string SqlStr)
-        {
-            return DbEntry.Context.ExecuteList<T>(SqlStr);
-        }
-
-        public static DbObjectList<T> FindBySql(SqlEntry.SqlStatement Sql)
-        {
-            return DbEntry.Context.ExecuteList<T>(Sql);
-        }
-
-        public static DbObjectList<T> FindAll()
-        {
-            return FindAll(null);
-        }
-
-        public static DbObjectList<T> FindAll(OrderBy ob)
-        {
-            return DbEntry.From<T>().Where(null).OrderBy(ob).Select();
-        }
-
-        public static DbObjectList<T> Find(WhereCondition con)
-        {
-            return Find(con, null);
-        }
-
-        public static DbObjectList<T> Find(WhereCondition con, OrderBy ob)
-        {
-            return DbEntry.From<T>().Where(con).OrderBy(ob).Select();
-        }
-
-        public static T FindOne(WhereCondition con)
-        {
-            return FindOne(con, null);
-        }
-
-        public static T FindOne(WhereCondition con, OrderBy ob)
-        {
-            return DbEntry.GetObject<T>(con, ob);
-        }
-
-        public static DbObjectList<T> FindRecent(int Count)
-        {
-            return DbEntry.From<T>().Where(null).OrderBy((DESC)"Id").Range(1, Count).Select();
-        }
-
-        public static long GetCount(WhereCondition con)
-        {
-            return DbEntry.From<T>().Where(con).GetCount();
-        }
-
-        public static T New()
-        {
-            return DynamicObject.NewObject<T>();
-        }
-
-        public static T New(params object[] os)
-        {
-            return DynamicObject.NewObject<T>(os);
-        }
-
-        public static void DeleteAll()
-        {
-            DeleteAll(null);
-        }
-
-        public static void DeleteAll(WhereCondition con)
-        {
-            DbEntry.Delete<T>(con);
         }
     }
 
