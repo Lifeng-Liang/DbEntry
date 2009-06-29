a91d0a95da311ddf67e100557e109da450602947
SND\lifeng_cp <SND\lifeng_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2009-06-29 12:39:52 +0000

    work item: 23293
    git-svn-id: https://dbentry.svn.codeplex.com/svn@55649 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- src/Lephone.Data/Definition/CreateTableListAttribute.cs
+++ src/Lephone.Data/Definition/CreateTableListAttribute.cs
@@ -7,6 +7,27 @@ namespace Lephone.Data.Definition
     {
         public Type[] Types;
 
+        public CreateTableListAttribute(Type type)
+        {
+            Types = new Type[1];
+            Types[0] = type;
+        }
+
+        public CreateTableListAttribute(Type type1, Type type2)
+        {
+            Types = new Type[2];
+            Types[0] = type1;
+            Types[1] = type2;
+        }
+
+        public CreateTableListAttribute(Type type1, Type type2, Type type3)
+        {
+            Types = new Type[3];
+            Types[0] = type1;
+            Types[1] = type2;
+            Types[2] = type3;
+        }
+
         public CreateTableListAttribute(params Type[] types)
         {
             Types = types;
