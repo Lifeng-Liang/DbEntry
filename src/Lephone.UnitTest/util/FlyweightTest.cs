2255484b947ba9a35e2bdabcecc7dadb44e219e7
SND\lifeng_cp <SND\lifeng_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2009-11-12 11:41:45 +0000

    work item: 25317
    git-svn-id: https://dbentry.svn.codeplex.com/svn@61301 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ src/Lephone.UnitTest/util/FlyweightTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using Lephone.Util;
+using NUnit.Framework;
+
+namespace Lephone.UnitTest.util
+{
+    [TestFixture]
+    public class FlyweightTest
+    {
+        public class Flyweight : FlyweightBase<string, Flyweight>
+        {
+            public static int Count;
+
+            protected override void Init(string t)
+            {
+                Count++;
+                Thread.Sleep(1000);
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Flyweight.Count = 0;
+
+            var t1 = new Thread(DoWork);
+            var t2 = new Thread(DoWork);
+
+            t1.Start();
+            t2.Start();
+
+            Thread.Sleep(2000);
+            Assert.AreEqual(1, Flyweight.Count);
+        }
+
+        public static void DoWork()
+        {
+            Flyweight.GetInstance("test");
+        }
+    }
+}
