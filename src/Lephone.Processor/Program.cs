54cbc5d40df554fc70e2be531c32e1fdbe96473d
SND\lifeng_cp <SND\lifeng_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2010-06-03 15:56:25 +0000

    work item: 27340
    git-svn-id: https://dbentry.svn.codeplex.com/svn@66535 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ src/Lephone.Processor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Mono.Cecil;
+
+namespace Lephone.Processor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Lephone.AssemblyProcessor ModelAssemblyName");
+                return;
+            }
+            var name = args[0];
+            var module = ModuleDefinition.ReadModule(name);
+
+            foreach (var type in module.Types)
+            {
+                if (type.IsDbModel())
+                {
+                    Console.WriteLine(type.FullName);
+                }
+            }
+
+            module.Write(name);
+        }
+    }
+}
---
+++ src/Lephone.Processor/TypeHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+
+namespace Lephone.Processor
+{
+    public static class TypeHelper
+    {
+        public static CustomAttribute GetCustomAttribute(MethodDefinition type, Type attributeType)
+        {
+            if (!type.HasCustomAttributes)
+                return null;
+
+            foreach (CustomAttribute attribute in type.CustomAttributes)
+            {
+                if (attribute.Constructor.DeclaringType.FullName != attributeType.FullName)
+                    continue;
+
+                return attribute;
+            }
+
+            return null;
+        }
+
+        public static bool IsDbModel(this TypeDefinition type)
+        {
+            return type.Interfaces.Any(p => p.FullName == "Lephone.Data.Definition.IDbObject");
+        }
+    }
+}
